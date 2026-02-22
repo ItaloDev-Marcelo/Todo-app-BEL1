5dd250896044645caab6d2c4dac538ac55eee755
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-22 16:44:46 -0300

    feat(server): add express,connecToDB,router and start the server with app.listen

---
+++ Server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectToDB = require('./Database/db');
+const router = require('./Routes/routes.todo')
+require('dotenv').config()
+app.use(express.json())
+
+app.use('/api/todos', router)
+connectToDB()
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
