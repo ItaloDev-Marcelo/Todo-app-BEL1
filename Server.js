25bfaf70356b2adda291e60119faddb7a1d4bd41
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-22 17:56:30 -0300

    docs: onlty more dotenv to line one

--- Server.js
+++ Server.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectToDB = require('./Database/db');
 const router = require('./Routes/routes.todo')
-require('dotenv').config()
 app.use(express.json())
 
 app.use('/api/todos', router)
 connectToDB()
 
+console.log(process.env.MONGO_URI)
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`)
