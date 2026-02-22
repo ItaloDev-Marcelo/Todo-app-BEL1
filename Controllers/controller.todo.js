b956d7dfafcf1e2f52eece9a2ab95f8c3db45352
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-22 16:50:47 -0300

    feat(Controller): creating addNewTodo and getAllTodos working on the others

---
+++ Controllers/controller.todo.js
@@ -0,0 +1,61 @@
+
+const Todo = require('../Models/Todo')
+
+const addNewTodo = async (req,res) => {
+  try {
+    const newTodoItem = req.body
+    const todo = await Todo.create(newTodoItem)
+     if(todo) {
+         res.status(201).json({
+        success: true,
+        message: 'Todo item created successfully',
+        data: todo
+    })
+     }
+  }catch(e) {
+     res.status(500).json({
+        success: false,
+        message: 'Failed to create todo item'
+     })
+  }
+}
+
+const updateOneTodo = async (req,res) => {
+    
+}
+
+const deleteATodo = async (req,res) => {
+    
+}
+
+const getAllTodos = async (req,res) => {
+    try {
+      const allTodos = await Todo.find({})
+      if(allTodos?.length > 0) {
+          res.status(200).json({
+            success: true,
+            message: 'Todo items retrieved successfully',
+            data: allTodos
+          })
+      }
+    }catch(e) {
+      console.log(e)
+      res.status(500).json({
+        success: false,
+        message: 'Failed to retrieve todo items'
+      })
+    }
+}
+
+const getOneTodo = async (req,res) => {
+
+}
+
+
+module.exports = {
+    addNewTodo,
+    updateOneTodo,
+    deleteATodo,
+    getAllTodos,
+    getOneTodo
+}
