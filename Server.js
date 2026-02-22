299dcab46df92a5e13e28d576ab888dc0ecc49e8
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-22 16:57:35 -0300

    refactor(PORT): change port 3000 to 5000

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const connectToDB = require('./Database/db');
 const router = require('./Routes/routes.todo')
 require('dotenv').config()
